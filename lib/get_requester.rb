6be2bc283f7dbec690b1964f139c1909c113f59d
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-06-18 00:55:32 +0300

    commit tree 082ee117bc58cf6b84231e3fa0cc60ae70c7344e

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,21 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+class GetRequester 
+    attr_accessor :url
+    def initialize url
+        @url = url
+    end
+    def get_response_body 
+        response = URI.parse(@url)
+        resp = Net::HTTP.get_response(response)
+        resp.body
+    end
+
+    def parse_json 
+        JSON.parse(get_response_body)
+    end
+
+end
